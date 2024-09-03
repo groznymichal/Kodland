32a2644152d5d5cee02d24843f6a1537b796bef0
groznymichal <kajtekmazur11@gmail.com>
2024-09-03 19:26:44 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+slowa = {
+    "LOL" : "odpowiedź na coś zabawnego",
+    "SHEESH" : "lekka dezaprobata",
+    "CRINLOL" : "odpowiedź na coś zabawnegoGE - coś dziwnego lub wstydliwego",
+    "ROFL" : "odpowiedź na żart",
+    "CREEPY" : "straszny, złowieszczy",
+    "AGGRO" : "stać się agresywnym/zły" 
+}
+slowo = input("podaj slowo")
+print(slowa[slowo])
